2ae50883725444e5baf581a3540a1d28e4d4871e
Leo Lu <leolu2@hotmail.com>
2025-06-26 22:44:43 -0400

    switch to gunicorn

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from waitress import serve
 
 """
 WSGI config for backend project.
@@ -11,13 +10,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-
-if __name__ == "__main__":
-    serve(application, host="0.0.0.0", port=8000)
--- backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-trap 'exit 0' SIGTERM SIGINT
-
-waitress-serve --port=8000 backend.wsgi:application &
-
-wait $!
