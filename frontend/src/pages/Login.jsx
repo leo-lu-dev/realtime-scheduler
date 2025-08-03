d52411abaddbd71a411e166761e513692ef7c60d
Leo Lu <leolu2@hotmail.com>
2025-08-03 04:06:03 -0400

    import and file cleanup

--- frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from '../components/AuthForm'
-
-function Login(){
-    return <AuthForm route='/api/token/' method='login'/>
-}
-
-export default Login
--- frontend/src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from '../components/AuthForm'
-
-function Register(){
-    return <AuthForm route='/api/register/' method='register'/>
-}
-
-export default Register
