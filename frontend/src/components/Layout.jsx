9f4eb957eadc3dd05245df26bef12adfc25feceb
Leo Lu <leolu2@hotmail.com>
2025-08-03 04:06:46 -0400

    layout component and styles

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,31 @@
+import { Outlet, Link } from 'react-router-dom';
+import { useAuth } from '../auth/AuthContext';
+import Header from './Header';
+import styles from '../styles/Layout.module.css';
+
+function Layout() {
+  const { isLoggedIn, isAuthLoaded } = useAuth();
+
+  if (!isAuthLoaded) return null;
+
+  return (
+    <>
+      <Header />
+      <div className={styles.layout}>
+        <div className={styles.body}>
+          {isLoggedIn && (
+            <div className={styles.sidebar}>
+              <Link to='/schedules' className='link'>Schedules</Link>
+            </div>
+          )}
+          <main className={styles.main}>
+            <Outlet />
+          </main>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default Layout;
